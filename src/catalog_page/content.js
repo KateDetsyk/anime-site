0d18c30689ffd4b08d230966232714724fbc3127
Kate Detsyk <detsyk.k@ucu.edu.ua>
2020-04-29 19:10:46 +0300

    t

--- src/catalog_page/content.js
+++ src/catalog_page/content.js
@@ -4,6 +4,9 @@ import './content.css';
 import logo0 from '../img/logo.png'
 import logo1 from '../img/logo1.jpg'
 import logo2 from '../img/logo2.jpg'
+import logo3 from '../img/logo3.jpg'
+import logo4 from '../img/logo4.jpg'
+import logo5 from '../img/logo5.jpg'
 import Navigation from '../navigation/navigation';
 
 
@@ -14,19 +17,24 @@ export default class Content extends React.Component {
 
     render() {
         return (
-        <body>
+        <div className="body">
            
         <div className="content">
         <Navigation></Navigation>
-            <table className="simpleCatalog">
+            <table className="simpleCatalog" width="100%">
             <tbody>
                 <tr>
                     <td><a href=""><img src={logo1} alt="Balance Unlimited" width="270" height="390"></img></a></td>
                     <td><a href=""><img src={logo0}  width="270" height="390"></img></a></td>
                     <td><a href=""><img src={logo2}  width="270" height="390"></img></a></td>
                 </tr>
+                <tr>
+                    <td><a href=""><img src={logo3} alt="Balance Unlimited" width="270" height="390"></img></a></td>
+                    <td><a href=""><img src={logo4}  width="270" height="390"></img></a></td>
+                    <td><a href=""><img src={logo5}  width="270" height="390"></img></a></td>
+                </tr>
             </tbody></table>
             </div>
-        </body>);
+        </div>);
     }
 }
