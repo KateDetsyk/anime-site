1ef5d679dbde7c45ab29e67e56c312e753f79f26
Kate Detsyk <detsyk.k@ucu.edu.ua>
2020-05-03 16:32:48 +0300

    add

--- src/ongoing_page/day_ongoing.js
+++ src/ongoing_page/day_ongoing.js
@@ -1,28 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-import './ongoing.css';
-import logo0 from './logo.png'
-import logo1 from '../img/logo1.jpg'
-import logo2 from '../img/logo2.jpg'
-import Navigation from '../navigation/navigation';
+// import './ongoing.css';
+// import logo0 from './logo.png'
+// import logo1 from '../img/logo1.jpg'
+// import logo2 from '../img/logo2.jpg'
+// import Navigation from '../navigation/navigation';
 
 
 
-export default class Ongoing extends React.Component {
+// export default class Ongoing extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <div className='day'>Monday</div>
-                <table className='test'>
-                    <tbody>
-                        <tr>
-                            <td className='goodcell'>
-                            <a href=""><img src={logo2} alt="Balance Unlimited" width="200" height="280"></img></a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>);
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <div className='day'>Monday</div>
+//                 <table className='test'>
+//                     <tbody>
+//                         <tr>
+//                             <td className='goodcell'>
+//                             <a href=""><img src={logo2} alt="Balance Unlimited" width="200" height="280"></img></a>
+//                             </td>
+//                         </tr>
+//                     </tbody>
+//                 </table>
+//             </div>);
+//     }
+// }
