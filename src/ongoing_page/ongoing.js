6185dd4eaa01db3b128f21f9226e087670643d80
Kate Detsyk <detsyk.k@ucu.edu.ua>
2020-05-10 22:51:08 +0300

    hw 2 changes 2

--- src/ongoing_page/ongoing.js
+++ src/ongoing_page/ongoing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './ongoing.css';
 
-import logo3 from '../img/1.jpg'
+import logo from '../img/1.jpg'
 import logo4 from '../img/0.jpg'
 import logo5 from '../img/2.jpg'
 
@@ -26,7 +26,7 @@ export default class Ongoing extends React.Component {
                 <div className='day'>Tuesday</div>
 
                 <div className='day'>Wednesday</div>
-                <DayContet logo={logo3} id="page1"></DayContet>
+                <DayContet logo={logo} id="page1"></DayContet>
 
                 <div className='day'>Thursday</div>
                 <DayContet logo={logo4} id="page0"></DayContet>
